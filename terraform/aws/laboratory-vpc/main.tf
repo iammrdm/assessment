910893427c737a60c90d18f3fc93f7f9a3baab60
imrdm <imrmendez1112@gmail.com>
2024-07-27 12:10:14 +0800

    Adjust storage

--- terraform/aws/laboratory-vpc/main.tf
+++ terraform/aws/laboratory-vpc/main.tf
@@ -118,7 +118,7 @@ resource "aws_instance" "laboratory_dev_ec2_instance" {
   associate_public_ip_address = true
 
   root_block_device {
-    volume_size = 30    # Specify the desired storage size in GB
+    volume_size = 100    # Specify the desired storage size in GB
     volume_type = "gp2" # General Purpose SSD
   }
 
